661699f9b1001ea3df197cea32f05feb17f25841
Enis Simsar <enisimsar@gmail.com>
2018-11-16 20:41:57 +0300

    Include header files.

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include "LPC407x_8x_177x_8x.h"
+
+#include "Library/GPIO.h"
+#include "Library/Timer.h"
+#include "Library/PWM.h"
+#include "Library/Joystick.h"
+#include "Library/LED.h"
+#include "Library/Ultrasonic.h"
+#include "Library/Wait.h"
+
 int milisecond = 1000;
 
 void update() {
