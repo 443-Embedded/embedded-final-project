c08a97452bcde4d1ef3b619adb2578839e630179
emirhansarac29 <emirhan.sarac29@gmail.com>
2018-11-20 19:11:41 +0300

    GPIO.h committed by Yusuf and Emirhan

--- Library/GPIO.c
+++ Library/GPIO.c
@@ -1,15 +1,17 @@
 #include "GPIO.h"
 
 void GPIO_Init() {
+	// Enable GPIO
 	PCONP |= 1 << 15;
 	
+	// Sets IOCON functions as GPIO for Joystick.
 	IOCON_LEFT &= ~(7);
 	IOCON_DOWN &= ~(7);
 	IOCON_RIGHT &= ~(7);
 	IOCON_CENTER &= ~(7);
 	IOCON_UP &= ~(7);
 	
-	// Motor Directions and LED pins
+	// Sets Motor Directions and LED pins
 	PORT0->DIR |= (1 << MOTOR_PINS[0][0] | 1 << MOTOR_PINS[0][1] | 1 << MOTOR_PINS[1][0] | 1 << MOTOR_PINS[1][1]);
 	PORT0->DIR |= (1 << LED_PINS[0] | 1 << LED_PINS[1] | 1 << LED_PINS[2] | 1 << LED_PINS[3]);
 
@@ -17,6 +19,7 @@ void GPIO_Init() {
 	PORT5->DIR &= ~(1 << 0 | 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4);
 }
 
+// Returns given pin value for GPIOs
 uint32_t GPIO_PIN_Read(GPIO_TypeDef* PORT,uint32_t MASK) {
 	return PORT->PIN & MASK;
 }
