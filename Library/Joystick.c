b5ea140d137890089b7ef089e4092320bf8e3301
emirhansarac29 <emirhan.sarac29@gmail.com>
2018-11-20 19:18:47 +0300

    Joystick.c commented by Yusuf and Emirhan

--- Library/Joystick.c
+++ Library/Joystick.c
@@ -1,5 +1,6 @@
 #include "Joystick.h"
 
+// Returns 1 if left joystick is pressed.
 uint8_t Joystick_Left_Pressed(void) {
 	if(GPIO_PIN_Read(JOYSTICK_PORT,JOYSTICK_LEFT_MASK) == 0) {
 		return 1;
@@ -7,6 +8,7 @@ uint8_t Joystick_Left_Pressed(void) {
 	return 0;
 }
 
+// Returns 1 if down joystick is pressed.
 uint8_t Joystick_Down_Pressed(void) {
 	if(GPIO_PIN_Read(JOYSTICK_PORT,JOYSTICK_DOWN_MASK) == 0) {
 		return 1;
@@ -14,6 +16,7 @@ uint8_t Joystick_Down_Pressed(void) {
 	return 0;
 }
 
+// Returns 1 if up joystick is pressed.
 uint8_t Joystick_Up_Pressed(void) {
 	if(GPIO_PIN_Read(JOYSTICK_PORT,JOYSTICK_UP_MASK) == 0) {
 		return 1;
@@ -21,6 +24,7 @@ uint8_t Joystick_Up_Pressed(void) {
 	return 0;
 }
 
+// Returns 1 if center joystick is pressed.
 uint8_t Joystick_Center_Pressed(void) {
 	if(GPIO_PIN_Read(JOYSTICK_PORT,JOYSTICK_CENTER_MASK) == 0) {
 		return 1;
@@ -28,6 +32,7 @@ uint8_t Joystick_Center_Pressed(void) {
 	return 0;
 }
 
+// Returns 1 if right joystick is pressed.
 uint8_t Joystick_Right_Pressed(void) {
 	if(GPIO_PIN_Read(JOYSTICK_PORT,JOYSTICK_RIGHT_MASK) == 0) {
 		return 1;
