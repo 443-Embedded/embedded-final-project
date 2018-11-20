e125d978d1476d85c359cfee44385947b6fb2c5d
emirhansarac29 <emirhan.sarac29@gmail.com>
2018-11-20 19:21:24 +0300

    LED.c commented

--- Library/LED.c
+++ Library/LED.c
@@ -1,10 +1,12 @@
 #include "LED.h"
 #include "PWM.h"
 
+// Clears values of pins which are connected to LEDs
 void LED_Init() {
 	PORT0->CLR = (1 << LED_PINS[0] |1 << LED_PINS[1] | 1 << LED_PINS[2] | 1 << LED_PINS[3]);
 }
 
+// Change the state of LED for a given parameter
 void LED_Change(uint32_t LED_NUMBER, uint32_t IS_ON) {
 	if(IS_ON) {
 		PORT0->SET = (1 << LED_PINS[LED_NUMBER]);
