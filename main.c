f113461a8c9370d674f9d07d9b9ce92d766bf366
Muhammed Fatih BALIN <m.f.balin@gmail.com>
2018-11-20 18:53:45 +0300

    Direction changes have been put before power chang

--- main.c
+++ main.c
@@ -38,42 +38,42 @@ void MOTOR_Direction(uint32_t MOTOR_TYPE, Motor_State state) {
 void update() {
 	if (Joystick_Center_Pressed()) {
 		TURN_LEFT_FLAG = TURN_RIGHT_FLAG = FORWARD_FLAG = BACKWARD_FLAG = 0;
-		PWM_MOTOR_Write(0, 0);
-		PWM_MOTOR_Write(0, 1);
 		MOTOR_Direction(0, STOP);
 		MOTOR_Direction(1, STOP);
+		PWM_MOTOR_Write(0, 0);
+		PWM_MOTOR_Write(0, 1);
 		LED_Adjuster(STOP_LED);
 	} else if (Joystick_Up_Pressed()) {
 		TURN_LEFT_FLAG = TURN_RIGHT_FLAG = BACKWARD_FLAG = 0;
 		FORWARD_FLAG = 1;
-		PWM_MOTOR_Write(ROBOT_SPEED, 0);
-		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		MOTOR_Direction(0, FORWARD);
 		MOTOR_Direction(1, FORWARD);
+		PWM_MOTOR_Write(ROBOT_SPEED, 0);
+		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		LED_Adjuster(FORWARD_LED);
 	} else if (Joystick_Down_Pressed()) {
 		TURN_LEFT_FLAG = TURN_RIGHT_FLAG = FORWARD_FLAG = 0;
 		BACKWARD_FLAG = 1;
-		PWM_MOTOR_Write(ROBOT_SPEED, 0);
-		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		MOTOR_Direction(0, BACKWARD);
 		MOTOR_Direction(1, BACKWARD);
+		PWM_MOTOR_Write(ROBOT_SPEED, 0);
+		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		LED_Adjuster(BACKWARD_LED);
 	} else if (Joystick_Right_Pressed()) {
 		TURN_LEFT_FLAG = BACKWARD_FLAG = FORWARD_FLAG = 0;
 		TURN_RIGHT_FLAG = 1;
-		PWM_MOTOR_Write(ROBOT_SPEED, 0);
-		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		MOTOR_Direction(1, BACKWARD);
 		MOTOR_Direction(0, FORWARD);
+		PWM_MOTOR_Write(ROBOT_SPEED, 0);
+		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		LED_Adjuster(RIGHT_BLINKER);
 	} else if (Joystick_Left_Pressed()) {
 		TURN_RIGHT_FLAG = BACKWARD_FLAG = FORWARD_FLAG = 0;
 		TURN_LEFT_FLAG = 1;
-		PWM_MOTOR_Write(ROBOT_SPEED, 0);
-		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		MOTOR_Direction(1, FORWARD);
 		MOTOR_Direction(0, BACKWARD);
+		PWM_MOTOR_Write(ROBOT_SPEED, 0);
+		PWM_MOTOR_Write(ROBOT_SPEED, 1);
 		LED_Adjuster(LEFT_BLINKER);
 	}
 }
