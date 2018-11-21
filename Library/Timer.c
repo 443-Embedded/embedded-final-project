1bd219571d64f0049ee2a2c54ec5158682ebd796
Enis Simsar <enisimsar@gmail.com>
2018-11-21 09:59:23 +0300

    Disable and enable interrupts for Timer2. Chhange var name.

--- Library/Timer.c
+++ Library/Timer.c
@@ -10,8 +10,8 @@ void Timer_Init() {
 	//Set the function of P0_4 to T2_CAP0 
 	IOCON_TIMER2_CAP0 |= 3;
 	
-	//Enable capture rising edge and interrupt for both CAP0 and CAP1
-	TIMER2->CCR |= 1 << 0 | 1 << 2 | 1 << 3 | 1 << 5;
+	//Enable capture rising edge for both CAP0 and CAP1
+	TIMER2->CCR |= 1 << 0 | 1 << 3;
 	
 	//Disable Timer Counter and Prescale Counter for Timer2.
 	TIMER2->TCR &= ~(1 << 0);
@@ -101,6 +101,9 @@ void TIMER2_Start() {
 	//Reset tacho counts
 	tacho[0] = tacho[1] = 0;
 	
+	//Enable interrupts for both CAP0 and CAP1
+	TIMER2->CCR |= 1 << 2 | 1 << 5;
+	
 	//Reset Timer Counter and Prescale Counter for Timer2.
 	TIMER2->TCR |= (1 << 1);
 	
@@ -113,6 +116,9 @@ void TIMER2_Start() {
 void TIMER2_Stop() {
 	//Stop Timer2 Counter 
 	TIMER2->TCR &= ~(1 << 0);
+	
+	//Disable interrupts for both CAP0 and CAP1
+	TIMER2->CCR &= ~(1 << 2 | 1 << 5);
 }
 /*
 * Depending on the capture register, we increment tacho count of corresponding motor. If a motors completes its 
@@ -123,9 +129,9 @@ void TIMER2_IRQHandler() {
 	if(TIMER2->IR & (1 << 5))
 		IR_idx = 5;
 	tacho[IR_idx - 4]++;
-	if(tacho[IR_idx - 4] >= ROTATION_NUMBER_FOR_90_DEGREE * 6) {
+	if(tacho[IR_idx - 4] >= HOLE_NUMBER_FOR_90_DEGREE) {
 		MOTOR_Direction(IR_idx - 4, STOP);
-		if(tacho[5 - IR_idx] >= ROTATION_NUMBER_FOR_90_DEGREE * 6)
+		if(tacho[5 - IR_idx] >= HOLE_NUMBER_FOR_90_DEGREE)
 			LED_Adjuster(STOP_LED);
 	}
 	//Clear the interrupt flag for CAP channel event
